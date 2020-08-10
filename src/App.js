b6b7921d32b9c35c911fec08f9ffecc55af65b75
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2020-08-10 21:59:37 +0200

    ADD: Get movies with images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row.js';
+import requests from './requests.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Baptiste ! Let's build Netflix clone.</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
     </div>
   );
 }
